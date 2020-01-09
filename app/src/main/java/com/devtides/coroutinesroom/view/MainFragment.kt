e258ac47a12fc373c8de5492263a0689f75424bc
al <alfredo.yap@stu.bmcc.cuny.edu>
2020-01-09 17:24:26 -0500

    chap5 - MainFragment

--- app/src/main/java/com/devtides/coroutinesroom/view/MainFragment.kt
+++ app/src/main/java/com/devtides/coroutinesroom/view/MainFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.devtides.coroutinesroom.R
 import com.devtides.coroutinesroom.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.fragment_main.*
@@ -38,21 +39,37 @@ class MainFragment : Fragment() {
 
     fun observeViewModel() {
         viewModel.signout.observe(this, Observer {
-
+            Toast.makeText(activity, "Signed out", Toast.LENGTH_SHORT).show()
+            goToSignupSreen()
         })
         viewModel.userDeleted.observe(this, Observer {
-
+            Toast.makeText(activity, "User deleted", Toast.LENGTH_SHORT).show()
+            goToSignupSreen()
         })
+    }
+
+    private fun goToSignupSreen() {
+        this.findNavController().navigate(MainFragmentDirections.actionGoToSignup())
     }
 
     private fun onSignout() {
-        val action = MainFragmentDirections.actionGoToSignup()
-        Navigation.findNavController(usernameTV).navigate(action)
+//        val action = MainFragmentDirections.actionGoToSignup()
+//        Navigation.findNavController(usernameTV).navigate(action)
+        viewModel.onSignout()
     }
 
     private fun onDelete() {
-        val action = MainFragmentDirections.actionGoToSignup()
-        Navigation.findNavController(usernameTV).navigate(action)
+//        val action = MainFragmentDirections.actionGoToSignup()
+//        Navigation.findNavController(usernameTV).navigate(action)
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle("Delete user")
+                .setMessage("Are you sure you want to delete this user?")
+                .setPositiveButton("Yes") { p0, p1 -> viewModel.onDeleteUser() }
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show()
+        }
     }
 
 }
